83b6182d73d1eeaa71ab88a7bfbcc75220b75c30
diana2456 <115098463+diana2456@users.noreply.github.com>
2022-10-18 14:56:39 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,18 @@ import java.util.ListIterator;
 public class Main {
     public static void main(String[] args) {
         ArrayList<String> A = new ArrayList<>();
-        A.add("н");
-        A.add("мир");
-        A.add("сумка");
-        A.add("желатин");
-        A.add("бижутерия");
+        A.add("РЅ");
+        A.add("РјРёСЂ");
+        A.add("СЃСѓРјРєР°");
+        A.add("Р¶РµР»Р°С‚РёРЅ");
+        A.add("Р±РёР¶СѓС‚РµСЂРёСЏ");
         System.out.println(A);
         ArrayList<String> B = new ArrayList<>();
-        B.add("литература");
-        B.add("конфетка");
-        B.add("осенью");
-        B.add("клей");
-        B.add("ка");
+        B.add("Р»РёС‚РµСЂР°С‚СѓСЂР°");
+        B.add("РєРѕРЅС„РµС‚РєР°");
+        B.add("РѕСЃРµРЅСЊСЋ");
+        B.add("РєР»РµР№");
+        B.add("РєР°");
         Collections.reverse(B);
         ArrayList<String> C = new ArrayList<>();
         ListIterator<String> listIter = A.listIterator();
